9350593241c989c60e5bb8ddc2ff6dd2b2a04227
Caroline Kim <carolinejkim@berkeley.edu>
2021-08-04 21:12:11 -0700

    Setting up home page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./Home";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/users">
-          <h2>Users</h2>
-        </Route>
-        <Route path="/actions">
-          <h2>Actions</h2>
-        </Route>
-      </div>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/users">
+        <h2>Users</h2>
+      </Route>
+      <Route path="/actions">
+        <h2>Actions</h2>
+      </Route>
     </Router>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React from 'react';
+import './Home.css';
+import { BsHeart, BsLaptop } from 'react-icons/bs';
+import { FaProjectDiagram } from 'react-icons/fa';
+import { CgGirl } from 'react-icons/cg';
+import { VscSymbolMisc } from 'react-icons/vsc';
+import { AiOutlineMail } from 'react-icons/ai';
 
 const Home = () => {
   return (
-    <h2>Home!</h2>
+    <div className="homeContainer">
+      <div className="aboutContainer">
+        <div className="about">
+          <CgGirl />
+          <br/>
+          <span>ABOUT</span>
+        </div>
+      </div>
+      <div className="experienceContainer">
+        <div>
+          <BsLaptop />
+          <br/>
+          <span>EXPERIENCE</span>
+        </div>
+      </div>
+      <div className="projectContainer">
+        <div>
+          <FaProjectDiagram />
+          <br/>
+          <span>PROJECTS</span>
+        </div>
+      </div>
+      <div className="loveContainer">
+        <div>
+          <BsHeart />
+          <br/>
+          <span>LOVE</span>
+        </div>
+      </div>
+      <div className="miscContainer">
+        <div>
+          <VscSymbolMisc />
+          <br/>
+          <span>MISC</span>
+        </div>
+      </div>
+      <div className="contactContainer">
+        <div>
+          <AiOutlineMail />
+          <br/>
+          <span>CONTACT</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
